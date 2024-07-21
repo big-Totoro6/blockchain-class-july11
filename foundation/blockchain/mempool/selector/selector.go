caa19a450db77b58ffd1395f9597c0f2afc72bec
bigtotoro <zhangjason@gmail.com>
2024-07-21 21:06:50 +0800

    增加map[database.AccountID][]database.BlockTx代码解析

--- foundation/blockchain/mempool/selector/selector.go
+++ foundation/blockchain/mempool/selector/selector.go
@@ -24,6 +24,11 @@ var strategies = map[string]Func{
 // account and selects howMany of them in an order based on the functions
 // strategy. All selector functions MUST respect nonce ordering. Receiving 0
 // for howMany must return all the transactions in the strategies ordering.
+// Func 定义了一个函数类型，该函数接受一个按账户分组的交易内存池，并根据其策略选择多少个交易。
+// 所有选择器函数必须遵守nonce顺序。当 howMany 参数为 0 时，必须按照策略的顺序返回所有交易。
+// transactions：一个映射，键类型为 database.AccountID，值为 []database.BlockTx，表示按账户分组的交易数据。每个账户ID对应一个交易数据列表。
+// return 函数返回一个 []database.BlockTx 类型的切片，即选定的交易数据列表。
+// 使用 map[KeyType]ValueType 的形式来定义映射类型
 type Func func(transactions map[database.AccountID][]database.BlockTx, howMany int) []database.BlockTx
 
 // Retrieve returns the specified select strategy function.
