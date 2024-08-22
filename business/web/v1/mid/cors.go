3435ee2f06bfaf047d170ce0127bab4fc47496b3
bigtotoro <zhangjason@gmail.com>
2024-08-22 10:25:49 +0800

    feat:重新添加gin依赖 并使cors兼容gin

--- business/web/v1/mid/cors.go
+++ business/web/v1/mid/cors.go
@@ -1,32 +1,25 @@
 package mid
 
 import (
-	"context"
+	"github.com/gin-gonic/gin"
 	"net/http"
-
-	"github.com/ardanlabs/blockchain/foundation/web"
 )
 
 // Cors sets the response headers needed for Cross-Origin Resource Sharing
-func Cors(origin string) web.Middleware {
+func Cors(origin string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// Set the CORS headers to the response.
+		c.Header("Access-Control-Allow-Origin", origin)
+		c.Header("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS")
+		c.Header("Access-Control-Allow-Headers", "Origin, Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
-	// This is the actual middleware function to be executed.
-	m := func(handler web.Handler) web.Handler {
-
-		// Create the handler that will be attached in the middleware chain.
-		h := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
-
-			// Set the CORS headers to the response.
-			w.Header().Set("Access-Control-Allow-Origin", origin)
-			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS")
-			w.Header().Set("Access-Control-Allow-Headers", "Origin, Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-
-			// Call the next handler.
-			return handler(ctx, w, r)
+		// If it's a preflight request, return 200 status and stop further processing.
+		if c.Request.Method == http.MethodOptions {
+			c.AbortWithStatus(http.StatusOK)
+			return
 		}
 
-		return h
+		// Call the next handler.
+		c.Next()
 	}
-
-	return m
 }
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/ardanlabs/conf/v3 v3.1.6
 	github.com/dimfeld/httptreemux/v5 v5.5.0
 	github.com/ethereum/go-ethereum v1.12.0
+	github.com/gin-gonic/gin v1.10.0
 	github.com/go-playground/locales v0.14.1
 	github.com/go-playground/universal-translator v0.18.1
 	github.com/go-playground/validator/v10 v10.20.0
@@ -23,7 +24,6 @@ require (
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.2.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.10.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
 	github.com/holiman/uint256 v1.2.3 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
